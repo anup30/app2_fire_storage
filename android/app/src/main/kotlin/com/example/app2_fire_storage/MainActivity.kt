78807ea4bd245c187db24e869eec2b738323a34e
anup30 <anupbarua30@gmail.com>
2024-04-12 01:44:47 +0600

    commit 1.0

---
+++ android/app/src/main/kotlin/com/example/app2_fire_storage/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app2_fire_storage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+// firebase storage
+// assignment module 20 (firebase):
+// upload images to firebase storage, and show in gridview
+
+import 'package:app2_fire_storage/upload_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main()async{
+  WidgetsFlutterBinding.ensureInitialized(); // <-- for firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const UploadScreen(),
+      theme: ThemeData(
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            backgroundColor: Colors.blue[500],
+            foregroundColor: Colors.green[100],
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            textStyle: const TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
